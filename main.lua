2328ca5b99f59cc4eb7b22ee184ee254d1db5020
VictoriousMakesThings <57701973+VictoriousMakesThings@users.noreply.github.com>
2020-03-20 14:32:24 +1100

    Update main.lua
    Fixed potential LUA issue when there are no MS bids.
    Fixed potential race condition with names being replaced by spell names from other addons.
    Added `!bid cancel` to retract bids.
    Added `!help` to receive command rundown.
    `!open` will now parrot to use `!bid ms` and `!bid os`.
    Send error to players if `!bis` received instead of `!bid`

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ epgp:SetScript("OnEvent", function(self, event, message, sender, ...)
   command = strlower(command)
   if command == "!bid" then do_bid(arguments, sender)
   elseif command == "!open" then open_bids(arguments, sender)
+  elseif command == "!bis" then epgp_unknown_command(arguments, sender)
+  elseif command == "!help" then epgp_help_command(arguments, sender)
   elseif command == "!close" then close_bids(sender)
   end
 end)
@@ -18,6 +20,14 @@ function is_lootmaster(name)
     return true;
   end
   return false;
+end
+
+function epgp_unknown_command(arguments, player)
+  SendChatMessage("IntensityEPGP: Unknown command. Type !bid ms, !bid os, or !bid cancel.", "WHISPER", nil, player)
+end
+
+function epgp_help_command(arguments, player)
+  SendChatMessage("IntensityEPGP: After bidding has opened (look for the raid warning), type !bid ms (for main spec), !bid os (for off spec), or !bid cancel (to retract bids).", "WHISPER", nil, player)
 end
 
 function open_bids(item, player)
@@ -37,7 +47,7 @@ function open_bids(item, player)
   end
 
   bid_item = item;
-  SendChatMessage("IntensityEPGP: "..item.." open for bidding!", "RAID_WARNING");
+  SendChatMessage("IntensityEPGP: "..item.." open for bidding! Type !bid ms (for main spec) or !bid os (for off spec)", "RAID_WARNING");
 end
 
 function close_bids(player)
@@ -53,7 +63,7 @@ function close_bids(player)
   ms_bidders_text = ""
   os_bidders_text = ""
   for i=1,getn(ms_bidders) do
-    name = strsplit("-", ms_bidders[i])
+    local name = ms_bidders[i];
     if strlen(ms_bidders_text)>0 then
       ms_bidders_text = ms_bidders_text .. ", " .. name;
     else
@@ -62,8 +72,8 @@ function close_bids(player)
   end
 
   for i=1,getn(os_bidders) do
+    local name = os_bidders[i];
     if strlen(os_bidders_text)>0 then
-      name = strsplit("-", os_bidders[i])
       os_bidders_text = os_bidders_text .. ", " .. name;
     else
       os_bidders_text = name;
@@ -89,23 +99,44 @@ function close_bids(player)
   os_bidders = {};
 end
 
-function do_bid(spec, player)
+function do_bid(spec, gpPlayer)
   -- can_bid
   if bid_item == nil then
-    SendChatMessage("IntensityEPGP: No item is currently open for bidding.", "WHISPER", nil, player)
+    SendChatMessage("IntensityEPGP: No item is currently open for bidding.", "WHISPER", nil, gpPlayer)
     return;
   end
 
   spec = strlower(spec);
+  gpPlayer = strsplit("-", gpPlayer);
 
   if spec == "ms" or spec == "main" then
-    SendChatMessage("IntensityEPGP: Main spec bid accepted for "..bid_item..".", "WHISPER", nil, player)
-    tinsert(ms_bidders, player)
+    SendChatMessage("IntensityEPGP: Main spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
+    tinsert(ms_bidders, gpPlayer)
   elseif spec == "os" or spec == "off" or spec == "offspec" then
-    SendChatMessage("IntensityEPGP: Off spec bid accepted for "..bid_item..".", "WHISPER", nil, player)
-    tinsert(os_bidders, player)
+    SendChatMessage("IntensityEPGP: Off spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
+    tinsert(os_bidders, gpPlayer)
+  elseif spec == "cancel" then
+    if tContains(ms_bidders, gpPlayer) then
+      for i=1,getn(ms_bidders) do
+        if ms_bidders[i] == gpPlayer then
+          tremove(ms_bidders, i)
+          SendChatMessage("IntensityEPGP: Your bid has successfully been withdrawn.", "WHISPER", nil, gpPlayer)
+          break
+        end
+      end
+    elseif tContains(os_bidders, gpPlayer) then
+      for i=1,getn(os_bidders) do
+        if os_bidders[i] == gpPlayer then
+          tremove(os_bidders, i)
+          SendChatMessage("IntensityEPGP: Your bid has successfully been withdrawn.", "WHISPER", nil, gpPlayer)
+          break
+        end
+      end
+    else
+      SendChatMessage("IntensityEPGP: You do not appear to have a bid active on the current item.", "WHISPER", nil, gpPlayer)
+    end
   else
-    SendChatMessage("IntensityEPGP: Bid not accepted for "..bid_item..". Please specify spec, e.g `!bid ms` or `!bid os`.", "WHISPER", nil, player)
+    SendChatMessage("IntensityEPGP: Bid not accepted for "..bid_item..". Please specify spec, e.g `!bid ms` or `!bid os`.", "WHISPER", nil, gpPlayer)
     return;
   end
 end
