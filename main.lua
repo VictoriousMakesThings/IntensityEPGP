5ae461a07cd345f16d10bfbe922851faab6df254
VictoriousMakesThings <57701973+VictoriousMakesThings@users.noreply.github.com>
2020-04-28 16:55:42 +1000

    Update main.lua
    Restore close_bids command
    Support alert timer termination in close_bids
    Split into end_bids command
    Tidy up !bid cancel functionality
    Prevent duplicate MS/OS bids

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 local epgp = CreateFrame("Frame")
---epgp:RegisterEvent("CHAT_MSG_GUILD")
---epgp:RegisterEvent("CHAT_MSG_WHISPER")
 epgp:RegisterEvent("CHAT_MSG_RAID")
 epgp:RegisterEvent("CHAT_MSG_RAID_LEADER")
 epgp:SetScript("OnEvent", function(self, event, message, sender, ...)
@@ -11,16 +9,15 @@ epgp:SetScript("OnEvent", function(self, event, message, sender, ...)
   elseif command == "!open" then open_bids(arguments, sender)
   elseif command == "!bis" then epgp_unknown_command(arguments, sender)
   elseif command == "!help" then epgp_help_command(arguments, sender)
-  -- elseif command == "!close" then close_bids(sender)
+  elseif command == "!close" then close_bids(sender)
   end
 end)
 
-function is_lootmaster(name)
-  -- This only works if in range of the party/raid member, too limited for general use
-  --local lootcaller = strsplit("-", name);
-  --local guildName, guildRankName, guildRankIndex = GetGuildInfo(lootcaller);
-  --if guildName ~= nil and guildRankIndex <= 1 then
+function can_bid(name)
+  return true;
+end
 
+function is_lootmaster(name)
   for i=1,GetNumGuildMembers() do
     guildname,rank,rankIndex=GetGuildRosterInfo(i);
     if guildname==name and rankIndex <= 1 then
@@ -87,11 +84,11 @@ function open_bids(gp_string, player)
   -- This should be what needs to be used
   -- C_ChatInfo.SendAddonMessage("iEPGP", "open\t" .. gp_item_link .. "\t" .. gp_amount .. "\t" .. gp_count .. "\t" .. gp_duration, "RAID")
 
-  C_Timer.After(gp_duration - 10, alert4)
-  C_Timer.After(gp_duration - 3, alert3)
-  C_Timer.After(gp_duration - 2, alert2)
-  C_Timer.After(gp_duration - 1, alert1)
-  C_Timer.After(gp_duration, close_bids)
+  iepgp_a4 = C_Timer.NewTimer(gp_duration - 10, alert4)
+  iepgp_a3 = C_Timer.NewTimer(gp_duration - 3, alert3)
+  iepgp_a2 = C_Timer.NewTimer(gp_duration - 2, alert2)
+  iepgp_a1 = C_Timer.NewTimer(gp_duration - 1, alert1)
+  iepgp_a0 = C_Timer.NewTimer(gp_duration, end_bids)
 end
 
 function alert4()
@@ -110,8 +107,18 @@ function alert1()
   SendChatMessage("iEPGP: Closing in 1", "RAID");
 end
 
+function terminate_alert_timers()
+  print(Terminating)
+  print(iepgp_a4)
+  if iepgp_a4 ~= nil then iepgp_a4:Cancel() end
+  if iepgp_a3 ~= nil then iepgp_a3:Cancel() end
+  if iepgp_a2 ~= nil then iepgp_a2:Cancel() end
+  if iepgp_a1 ~= nil then iepgp_a1:Cancel() end
+  if iepgp_a0 ~= nil then iepgp_a0:Cancel() end
+end
+
 function close_bids(player)
-  if not is_lootmaster(player) or player ~= nil then
+  if not is_lootmaster(player) or player == nil then
     return;
   end
 
@@ -120,6 +127,11 @@ function close_bids(player)
     return;
   end
 
+  terminate_alert_timers()
+  end_bids()
+end
+
+function end_bids()
   ms_bidders_text = ""
   os_bidders_text = ""
   for i=1,getn(ms_bidders) do
@@ -139,9 +151,7 @@ function close_bids(player)
       os_bidders_text = name;
     end
   end
-
   
-
   if (ms_bidders_text ~= "") then
     SendChatMessage("iEPGP: MS bids for "..bid_item..": "..ms_bidders_text, "RAID");
   elseif (os_bidders_text ~= "") then
@@ -157,8 +167,25 @@ function close_bids(player)
   os_bidders = {};
 end
 
+function cancel_bid(bidders, gpPlayer)
+  cleaned = false;
+  if tContains(bidders, gpPlayer) then
+    for i=1,getn(bidders) do
+      if bidders[i] == gpPlayer then
+        tremove(bidders, i)
+        cleaned = true;
+        break
+      end
+    end
+  end
+  return cleaned, bidders;
+end
+
 function do_bid(spec, gpPlayer)
-  -- can_bid
+  if not can_bid(player) or gpPlayer == nil then
+    return;
+  end
+
   if bid_item == nil then
     SendChatMessage("iEPGP: No item is currently open for bidding.", "WHISPER", nil, gpPlayer)
     return;
@@ -168,28 +195,24 @@ function do_bid(spec, gpPlayer)
   gpPlayer = strsplit("-", gpPlayer);
 
   if spec == "ms" or spec == "main" then
-    SendChatMessage("iEPGP: Main spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
-    tinsert(ms_bidders, gpPlayer)
-  elseif spec == "os" or spec == "off" or spec == "offspec" then
-    SendChatMessage("iEPGP: Off spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
-    tinsert(os_bidders, gpPlayer)
-  elseif spec == "cancel" then
     if tContains(ms_bidders, gpPlayer) then
-      for i=1,getn(ms_bidders) do
-        if ms_bidders[i] == gpPlayer then
-          tremove(ms_bidders, i)
-          SendChatMessage("iEPGP: Your bid has successfully been withdrawn.", "WHISPER", nil, gpPlayer)
-          break
-        end
-      end
-    elseif tContains(os_bidders, gpPlayer) then
-      for i=1,getn(os_bidders) do
-        if os_bidders[i] == gpPlayer then
-          tremove(os_bidders, i)
-          SendChatMessage("iEPGP: Your bid has successfully been withdrawn.", "WHISPER", nil, gpPlayer)
-          break
-        end
-      end
+      SendChatMessage("iEPGP: You've already bid main spec on "..bid_item..".", "WHISPER", nil, gpPlayer)
+    else
+      SendChatMessage("iEPGP: Main spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
+      tinsert(ms_bidders, gpPlayer)
+    end
+  elseif spec == "os" or spec == "off" or spec == "offspec" then
+    if tContains(os_bidders, gpPlayer) then
+      SendChatMessage("iEPGP: You've already bid off spec on "..bid_item..".", "WHISPER", nil, gpPlayer)
+    else
+      SendChatMessage("iEPGP: Off spec bid accepted for "..bid_item..".", "WHISPER", nil, gpPlayer)
+      tinsert(os_bidders, gpPlayer)
+    end
+  elseif spec == "cancel" then
+    local cleaned_ms, ms_bidders = cancel_bid(ms_bidders, gpPlayer);
+    local cleaned_os, os_bidders = cancel_bid(os_bidders, gpPlayer);
+    if cleaned_ms or cleaned_os then
+      SendChatMessage("iEPGP: Your bid has successfully been withdrawn.", "WHISPER", nil, gpPlayer)
     else
       SendChatMessage("iEPGP: You do not appear to have a bid active on the current item.", "WHISPER", nil, gpPlayer)
     end
